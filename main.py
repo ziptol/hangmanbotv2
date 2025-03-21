d60c140320f89bafb998653125b2092ed943600e
ziptol <zperry2002@gmail.com>
2025-03-21 00:02:08 -0400

    added birthdays

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 # imports
 import os
-import discord
+import discord 
 from dotenv import load_dotenv
+import asyncio
+from datetime import datetime, timedelta
 import hangmanGame
 import leaderboard as lb
+import birthdaybot as bd
 import random
 import connectfour
 
 # grab env variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+HOMECHANNEL = int(os.getenv('HOMECHANNEL'))
 
 # settings
 WORDLISTFILENAME = "assets/english-nouns.txt"
@@ -17,6 +21,7 @@ LEADERBOARDFILENAME = "leaderboard.txt"
 COMMANDPREFIX = "?"
 C4WINPOINTS = 3
 HMWINPOINTS = 1
+BIRTHDAYFILENAME = "birthdays.csv"
 
 class MyClient(discord.Client):
 
@@ -40,10 +45,53 @@ class MyClient(discord.Client):
         # games
         self.hangGame = None
         self.conGame = None
+        self.home_channel = None
 
     # on ready
     async def on_ready(self):
         print(f'Up in this shit. Logged in as {self.user}')
+        
+        # initialize birthday check task
+        asyncio.create_task(self.daily_task())
+
+        # set home channel
+        self.home_channel = self.get_channel(HOMECHANNEL)
+        if self.home_channel:
+            print(f"Home channel: {self.home_channel}")
+        else:
+            print("Home channel not found!")
+
+
+    # ---------------------------------------------------------------------------- #
+    #                                Daily Task                                    #
+    # ---------------------------------------------------------------------------- #
+    async def daily_task(self):
+        await self.wait_until_ready()  # Make sure bot is ready
+
+        while not self.is_closed():
+            now = datetime.now()
+            print(now)
+            # Set target time (5 seconds past midnight)
+            target_time = now.replace(hour=23, minute=56, second=0, microsecond=0)
+
+            # If time has passed today, schedule for tomorrow
+            if now >= target_time:
+                target_time += timedelta(days=1)
+
+            wait_seconds = (target_time - now).total_seconds()
+            print(f"Sleeping for {wait_seconds} seconds until birthday check.")
+            await asyncio.sleep(wait_seconds)
+
+            # Check for birthdays and send message
+            birthday_people = bd.check_birthdays(BIRTHDAYFILENAME)
+            if birthday_people and self.home_channel:
+                message = "🎉 Happy Birthday to: " + ", ".join(birthday_people) + "!"
+                await self.home_channel.send(message)
+            else:
+                print("No birthdays today.")
+
+            # Sleep for 24 hours
+            await asyncio.sleep(24 * 60 * 60)
 
     # ---------------------------------------------------------------------------- #
     #                                Command Handler                               #
@@ -72,13 +120,16 @@ class MyClient(discord.Client):
                 case "george":
                     await message.channel.send("https://youtu.be/AwZG9NLAGmg")
 
+                case "tag":
+                    await message.channel.send("https://www.youtube.com/watch?v=rV5A3uvxqQI")
+
                 case "dice":
                     file = discord.File("assets/fuckDice.jpg")
                     embed = discord.Embed()
                     embed.set_image(url="attachment://fuckDice.jpg")                    
                     await message.channel.send(file=file, embed=embed)
                 
-                case "nickwalkpie":
+                case "aria":
                     file = discord.File("assets/nwalkpie.png")
                     embed = discord.Embed()
                     embed.set_image(url="attachment://nwalkpie.png")                    
@@ -86,6 +137,51 @@ class MyClient(discord.Client):
                 
                 case "slime":
                     await message.channel.send("https://www.amazon.com/Awkward-Styles-Autism-T-Shirt-Toddler/dp/B08534BD4T/ref=sr_1_2_sspa?crid=W1GA96OIR4US&dib=eyJ2IjoiMSJ9.Hobyjo941-GLgqwSbPROaT54QEaW0pQjpmzzaw6q3ALr_SViuIaaAsAeoOxvZGbly-RinWi7GU_c-7lfQh_7HMcqXdAXaKLVce7wu67onmPswvt6nxml_14vYWlLwd-zZOQq0W04W8oGbroix9eSY7sjSA0PiWPNk7f11jMqIejgDoBxbkEFr8KDGkTIdN1GHRU40fHaS68_RQValzdBvFxl7WmlMtodsaiQpz-_pP7A-iIPMuTY7apmvvRn5qurvsNch7fv-Xjanhx5QSAIbPnCS6j9f5MPDRQjOmRNycc.MoHsDoOs9MjcJ6CqYXngr89Q7ttSMCw7TqbPxPqAZU8&dib_tag=se&keywords=autism+shirt&qid=1715286134&sprefix=autism+shirt%2Caps%2C107&sr=8-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1")
+                
+                # ----------------------------- Birthday Commands ----------------------------- #
+                case "addbday":
+                    parts = message.content.strip().split()
+
+                    if len(parts) != 3:
+                        await message.channel.send("fuck you. do addbirhtday <name> <MM-DD>")
+                        return
+
+                    username, date = parts[1], parts[2]
+
+                    try:
+                        bd.add_birthday(BIRTHDAYFILENAME, username, date)
+                        await message.channel.send(f"Added birthday: {username} - {date}")
+                    except ValueError as ve:
+                        await message.channel.send(f"Error: {ve}")
+                    except Exception as e:
+                        print("Failed to add birthday. Check server logs.")
+
+                case "listbdays":
+                    birthdays = bd.list_birthdays(BIRTHDAYFILENAME)
+                    if not birthdays:
+                        await message.channel.send("No birthdays found")
+                    else:
+                        formatted = "\n".join(birthdays)
+                        await message.channel.send(f"**Birthday List:**\n{formatted}")
+
+                case "removebday":
+                    parts = message.content.strip().split()
+
+                    if len(parts) != 2:
+                        await message.channel.send("fuck you. do removebday <name>")
+                        return
+
+                    username = parts[1]
+
+                    try:
+                        success = bd.remove_birthday(BIRTHDAYFILENAME, username)
+                        if success:
+                            await message.channel.send(f"Removed birthday entry for {username}.")
+                        else:
+                            await message.channel.send(f"No birthday found for {username}.")
+                    except Exception as e:
+                        print(f"Error: {e}")
+                        await message.channel.send("Failed to remove birthday. Check server logs.")
 
                 # ----------------------------- Hangman Commands ----------------------------- #
                 # Start a game of hangman, takes no parameters
@@ -183,11 +279,13 @@ class MyClient(discord.Client):
 
                 # Get leaderboard, takes no parameters
                 case "leaderboard":
+                    print("sending leaderboard!")
                     # display leaderboard 
                     leadermessage = "**Leaderboard:**\n"
                     for line in lb.getLeaderboard(LEADERBOARDFILENAME):
                         leadermessage += str(line[0])+": "+str(line[1])+"\n"
-
+                        
+                    print(leadermessage)
                     await message.channel.send(leadermessage)
 
                 
